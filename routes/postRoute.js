f11a208c7072f94a88957b97e25e12a164b3af03
Yaşar Emre Doğru <=>
2023-06-17 15:48:04 +0300

    edit

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router()
 const postController = require('../controllers/post/postController')
+const authMiddleware = require('../middlewares/verifyToken')
 
 router.get('/posts/:id',postController.PostById)
 router.get('/posts',postController.AllPosts)
+router.post('/newpost',authMiddleware.verifyToken,postController.newPost)
 router.get('/posts/user/:id',postController.getPostsByUserId)
+
 
 module.exports = router;
