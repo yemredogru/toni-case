cba43a69f110663ce2faa9a29938558fdcdd2663
Yaşar Emre Doğru <=>
2023-06-21 09:09:49 +0300

    already friend check middleware added

--- middlewares/friendRequestVerify.js
+++ middlewares/friendRequestVerify.js
@@ -21,12 +21,18 @@ async function ifRequest(req,res,next){
         return res.status(401).json({message:'User not found'})
     }
     else{
-        const is_request = await userModel.findOne({_id:req.user.user.id,"pending_requests.request_user_id":req.params.id})
-        if(!is_request){
+        const is_request = await userModel.findOne({_id:req.user.user.id,"pending_requests.request_user_id":req.params.id}) 
+        const is_friend = await userModel.findOne({_id:req.user.user.id,"friends.friend_id":req.params.id}) 
+        if(!is_request && !is_friend){
             next()
         }
         else{
-            return res.status(401).json({message:'Friend request already sent'})
+            if(is_request){
+                return res.status(401).json({message:'Friend request already sent'})
+            }
+            else{
+                return res.status(401).json({message:'Already friend'})
+            }
         }
     }
 }
